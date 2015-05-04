e7a51877f0e3bb8a3d25633195577025e43f8e6a
skylli <szlixiongsheng@163.com>
2015-05-04 21:21:27 +0800

    delete port src

--- ports/wiced/app/wilddog/skel/src/coap/build.mk
+++
@@ -1,7 +0,0 @@
-COAP_DIR=src/coap/
-INCLUDE_DIRS += $(COAP_DIR)
-CSRC += $(COAP_DIR)option.c
-CSRC += $(COAP_DIR)pdu.c
-CPPSRC +=
-ASRC +=
-
--- src/build.mk
+++
@@ -1,8 +0,0 @@
-
-SRC_DIR=src/
-INCLUDE_DIRS += $(SRC_DIR)
-CSRC += $(SRC_DIR)Wilddog.c
-CSRC += $(SRC_DIR)wilddog_debug.c
-CSRC += $(SRC_DIR)url_parser.c
-CPPSRC +=
-ASRC +=
