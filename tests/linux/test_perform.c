cce88843f94b3929285cf8b46cf3d230f984186a
dndxhej <dndxhej@gmail.com>
2015-07-15 22:05:41 +0800

    renew test_perform

--- tests/linux/test_perform.c
+++ tests/linux/test_perform.c
@@ -14,21 +14,8 @@
 
 int main(void)
 {
-	u8 m=0, n=0;
-	u8 tree_num[3] = {1,2,3};
-	u8 request_num[4] = {1,16,32,64};
 #ifdef WILDDOG_SELFTEST
-	ramtest_titile_printf();
-	//ramtest_handle(TREE_NUM,REQUEST_NUM);
-
-	for( m=0; m < 3; m++)
-	{
-		for( n=0; n < 4; n++)
-		{
-			ramtest_handle(tree_num[m],request_num[n]);
-		}
-	}
-	ramtest_end_printf();
+	performtest_all();
 #endif
 	return 0;
 }
